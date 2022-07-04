b3c5295e3366277dd12070ec325e79673f96e47b
Lucas Rodriguez <83020964+lrodriguez911@users.noreply.github.com>
2022-07-04 17:23:07 +0000

    add app use whit var

--- myApp.js
+++ myApp.js
@@ -2,10 +2,10 @@ let express = require('express');
 let app = express();
 console.log("Hello World");
 let htmlPath = __dirname + "/views/index.html";
-//let htmlStyle = __dirname + "/public";
+let htmlStyle = __dirname + "/public";
 /* app.get("/", (req, res) => res.send("Hello Express")); */
 app.get("/", (req, res)=>res.sendFile(htmlPath))
-app.use("/public",express.static(__dirname + "/public"))
+app.use("/public",express.static(htmlStyle))
 
 
 
