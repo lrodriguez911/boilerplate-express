bdda9b7115882ac7b9f03f84564311fd44df6a58
Lucas Rodriguez <83020964+lrodriguez911@users.noreply.github.com>
2022-07-04 16:18:40 +0000

    add app get whit middleware send

--- myApp.js
+++ myApp.js
@@ -1,6 +1,10 @@
 let express = require('express');
 let app = express();
-console.log("Hello World")
+console.log("Hello World");
+//let htmlPath = __dirame + "/views/index.html";
+app.get("/", (req, res) => res.send("Hello Express"));
+/* app.get("/", express.static("/public/styles.css")) */
+
 
 
 
