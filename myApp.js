6b8463314954bfdf503d7c943817df3251d8ff1c
lucasr <lrodriguez.arevalosc@gmail.com>
2022-07-05 12:35:54 -0300

    add var secrects enviroment variables, install dotenv, create a file .env and use conditional to show message lowercaso or uppercase

--- myApp.js
+++ myApp.js
@@ -1,44 +1,17 @@
-let express = require('express');
+require("dotenv").config();
+let express = require("express");
 let app = express();
-console.log("Hello World")
-let htmlPath = __dirname + "/views/index.html"
-let htmlStyle = __dirname + "/public"
+console.log("Hello World");
+const mySecret = process.env.MESSAGE_STYLE;
+let htmlPath = __dirname + "/views/index.html";
+let htmlStyle = __dirname + "/public";
 //app.get("/",(req, res) => res.send("Hello Express"))
-app.get("/",(req, res) => res.sendFile(htmlPath))
-app.use("/",express.static(htmlStyle))
+app.get("/", (req, res) => res.sendFile(htmlPath));
+app.use("/public", express.static(htmlStyle));
+app.get("/json", (req, res) => {
+  res.json(
+    {"message": (mySecret === "uppercase" ? "HELLO JSON" : "Hello json")}
+  );
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
