aa78a58d87a576eaa88786a49ef7ef7d928b2ed2
lucasr <lrodriguez.arevalosc@gmail.com>
2022-07-05 14:18:45 -0300

    add middleware whit app use

--- myApp.js
+++ myApp.js
@@ -6,12 +6,15 @@ const mySecret = process.env.MESSAGE_STYLE;
 let htmlPath = __dirname + "/views/index.html";
 let htmlStyle = __dirname + "/public";
 //app.get("/",(req, res) => res.send("Hello Express"))
+app.use("/", (req, res, next) =>{
+  console.log(`${req.method} ${req.path} - ${req.ip}`)
+next();
+})
 app.get("/", (req, res) => res.sendFile(htmlPath));
 app.use("/public", express.static(htmlStyle));
 app.get("/json", (req, res) => {
-  res.json(
-    {"message": (mySecret === "uppercase" ? "HELLO JSON" : "Hello json")}
-  );
+  let response = mySecret === "uppercase" ? "HELLO JSON" : "Hello json";
+  res.json({ "message": response });
 });
 
 module.exports = app;
