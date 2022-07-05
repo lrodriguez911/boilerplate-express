441a8f2f4326d6857e583263fa9a5f9b23cf9ef8
Lucas Rodriguez <83020964+lrodriguez911@users.noreply.github.com>
2022-07-05 17:20:25 +0000

    add middleware whit app use to show method, path and ip

--- myApp.js
+++ myApp.js
@@ -1,23 +1,32 @@
 let express = require('express');
 let app = express();
 console.log("Hello World");
-//require('dotenv').config()
-const mySecret = process.env.MESSAGE_STYLE;
 let htmlPath = __dirname + "/views/index.html";
 let htmlStyle = __dirname + "/public";
 /* app.get("/", (req, res) => res.send("Hello Express")); */
+app.use("/", (req, res, next) => {
+  console.log(`${req.method} ${req.path} - ${req.ip}`)
+  next()
+})
+
 app.get("/", (req, res) => res.sendFile(htmlPath))
+
 app.use("/public", express.static(htmlStyle))
+
 /* app.get("/json", (req, res) => {
   res.json({
     "message" : "Hello json"
   })
 }) */
-app.get("/json", (req, res) => { res.json(
+
+app.get("/json", (req, res) => { 
+  let response = process.env.MESSAGE_STYLE === "uppercase" ? "HELLO JSON" : "Hello json"
+  res.json(
     {
-    "message": (mySecret === "uppercase" ? "HELLO JSON" : "Hello json") 
+    "message": response
     })
 })
+
 
 
 
