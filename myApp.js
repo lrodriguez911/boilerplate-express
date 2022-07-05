4c52e6c5430b1505252e0c96bfe300f6e6b04bb0
Lucas Rodriguez <83020964+lrodriguez911@users.noreply.github.com>
2022-07-05 15:31:17 +0000

    using System environment variables to show a res.json

--- myApp.js
+++ myApp.js
@@ -1,11 +1,23 @@
 let express = require('express');
 let app = express();
 console.log("Hello World");
+//require('dotenv').config()
+const mySecret = process.env.MESSAGE_STYLE;
 let htmlPath = __dirname + "/views/index.html";
 let htmlStyle = __dirname + "/public";
 /* app.get("/", (req, res) => res.send("Hello Express")); */
-app.get("/", (req, res)=>res.sendFile(htmlPath))
-app.use("/public",express.static(htmlStyle))
+app.get("/", (req, res) => res.sendFile(htmlPath))
+app.use("/public", express.static(htmlStyle))
+/* app.get("/json", (req, res) => {
+  res.json({
+    "message" : "Hello json"
+  })
+}) */
+app.get("/json", (req, res) => { res.json(
+    {
+    "message": (mySecret === "uppercase" ? "HELLO JSON" : "Hello json") 
+    })
+})
 
 
 
@@ -42,4 +54,4 @@ app.use("/public",express.static(htmlStyle))
 
 
 
- module.exports = app;
+module.exports = app;
