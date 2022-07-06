31e28eebeb939b30ea4181ff9359d5cd706d0b9a
lucasr <lrodriguez.arevalosc@gmail.com>
2022-07-06 11:06:05 -0300

    create a url to show time and get route parameter input from client

--- myApp.js
+++ myApp.js
@@ -10,11 +10,21 @@ app.use("/", (req, res, next) =>{
   console.log(`${req.method} ${req.path} - ${req.ip}`)
 next();
 })
+app.get("/now", (req, res, next) => {
+req.time = new Date().toString();
+next();
+}, (req,res) => {
+  res.send({time : req.time})
+})
 app.get("/", (req, res) => res.sendFile(htmlPath));
 app.use("/public", express.static(htmlStyle));
 app.get("/json", (req, res) => {
   let response = mySecret === "uppercase" ? "HELLO JSON" : "Hello json";
   res.json({ "message": response });
 });
-
+//Get Route Parameter Input from the Client
+app.get("/:word/echo", (req,res,next)=>{
+let word = req.params.word
+res.send({echo:word})
+})
 module.exports = app;
