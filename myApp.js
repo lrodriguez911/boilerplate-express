e8a85a6e2797bf5c51670b308f18283699a14bc8
Lucas Rodriguez <83020964+lrodriguez911@users.noreply.github.com>
2022-07-06 17:18:29 +0000

    add body parser to use in mehtod post

--- myApp.js
+++ myApp.js
@@ -1,9 +1,12 @@
 let express = require('express');
 let app = express();
+let bodyParser = require('body-parser');
 console.log("Hello World");
 let htmlPath = __dirname + "/views/index.html";
 let htmlStyle = __dirname + "/public";
 /* app.get("/", (req, res) => res.send("Hello Express")); */
+app.use("/", bodyParser.urlencoded({extended: false}))
+app.post("/")
 app.use("/", (req, res, next) => {
   console.log(`${req.method} ${req.path} - ${req.ip}`)
   next()
@@ -38,7 +41,10 @@ app.get("/:word/echo", (req,res,next) => {
   let word = req.params.word;
   res.send({echo:word})
 })
-
+app.get("/name", (req, res, next) => {
+  let {first : firstname, last : lastname} = req.query;
+  res.json({name: `${firstname} ${lastname}`})
+})
 
 
 
