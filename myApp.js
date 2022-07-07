1355f20409f8bb8b418eef95dacf42f89e92ac82
Lucas Rodriguez <lrodriguez.arevalosc@gmail.com>
2022-07-07 16:15:00 +0000

    add method POST to send data to /name

--- myApp.js
+++ myApp.js
@@ -5,8 +5,8 @@ console.log("Hello World");
 let htmlPath = __dirname + "/views/index.html";
 let htmlStyle = __dirname + "/public";
 /* app.get("/", (req, res) => res.send("Hello Express")); */
-app.use("/", bodyParser.urlencoded({extended: false}))
-app.post("/")
+app.use("/", bodyParser.urlencoded({ extended: false }))
+//app.post("/")
 app.use("/", (req, res, next) => {
   console.log(`${req.method} ${req.path} - ${req.ip}`)
   next()
@@ -21,12 +21,11 @@ app.use("/public", express.static(htmlStyle))
     "message" : "Hello json"
   })
 }) */
-app.get("/json", (req, res) => { 
-  let response = process.env.MESSAGE_STYLE === "uppercase" ? "HELLO JSON" : "Hello json"
-  res.json(
-    {
-    "message": response
-    })
+app.get("/json", (req, res) => {
+  let response =
+    process.env.MESSAGE_STYLE === "uppercase" ?
+      "HELLO JSON" : "Hello json"
+  res.json({ "message": response})
 })
 //Chain Middleware to Create a Time Server
 //chain a middlwware to show time in url /now
@@ -34,18 +33,22 @@ app.get("/now", (req, res, next) => {
   req.time = new Date().toString();
   next();
 }, (req, res) => {
-  res.send({time : req.time})
+  res.send({ time: req.time })
 })
 //Get Route Parameter Input from the Client
-app.get("/:word/echo", (req,res,next) => {
+app.get("/:word/echo", (req, res, next) => {
   let word = req.params.word;
-  res.send({echo:word})
+  res.send({ echo: word })
 })
 app.get("/name", (req, res, next) => {
-  let {first : firstname, last : lastname} = req.query;
-  res.json({name: `${firstname} ${lastname}`})
+  let { first: firstname, last: lastname } = req.query;
+  res.json({ name: `${firstname} ${lastname}` })
 })
-
+// Get data from POST request
+app.post("/name", (req, res, next) => {
+  //let { first: firstname, last: lastname } = req.body;
+  res.json({name: `${req.body.first} ${req.body.last}`})
+})
 
 
 
