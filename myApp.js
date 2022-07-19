39e4f5efc7318087482aeed7c389836d2a9922b4
Lucas Rodriguez <lrodriguez.arevalosc@gmail.com>
2022-07-19 17:12:56 +0000

    add an html file whit sendFile and use static asstes whit app.use and express.static

--- myApp.js
+++ myApp.js
@@ -1,14 +1,19 @@
-let express = require('express');
-let app = express();
-
-
+const express = require('express');
+const app = express();
+let absPathHtml = __dirname + "/views/index.html";
+let absPathPublic = __dirname + "/public";
+// meet node console
 console.log("Hello World");
 
-app.get("/", (req, res) => res.send("Hello Express"))
+// start a working on express server
+/*app.get("/", (req, res) => res.send("Hello Express"))*/
 
 
+//server an html file
+app.get("/", (req, res) => res.sendFile(absPathHtml))
 
-
+// server static assets
+app.use("/public",express.static("public"))
 
 
 
